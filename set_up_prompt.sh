a3fa6ff52f414550a1058ef7e819e82f600bc67b
Arunkmar Mourougappane <7111785+arunkumar-mourougappane@users.noreply.github.com>
2020-04-17 21:05:09 -0500

    Updated config files for holy prompt additions

---
+++ set_up_prompt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+echo "Setting up apps for terminal"
+sudo apt install git gitg
+
+echo "Forcing Color prompt"
+sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/${USER}/.bashrc
+
+echo "Making prompt change a part of bash_profile ..."
+cat bashrc_prompt >> /home/${USER}/.bash_profile
+
+echo "Adding config files ..."
+cp  bashrc_sshagent /home/${USER}/.bashrc_sshagent
+cp  bashrc_gitcolor /home/${USER}/.bashrc_gitcolor
+
+echo "Settting up permissions ..."
+chmod +x /home/${USER}/.bashrc_*
