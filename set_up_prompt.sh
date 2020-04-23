da5fcdb9e6a10e6d4344775796a858a412dd7e8f
Arunkumar Mourougappane <111785+arunkumar-mourougappane@users.noreply.github.com>
2020-04-22 23:09:38 -0500

    Fixed syntax errors and permissions.

--- set_up_prompt.sh
+++ set_up_prompt.sh
@@ -8,8 +8,8 @@ sudo apt install git gitg
 echo "Forcing Color prompt"
 sed -i 's/#force_color_prompt=yes/force_color_prompt=yes/g' /home/${USER}/.bashrc
 
-echo "Making prompt change a part of bash_profile ..."
-cat bashrc_prompt >> /home/${USER}/.bash_profile
+echo "Making prompt change a part of bash_aliases ..."
+cat bashrc_prompt >> /home/${USER}/.bash_aliases
 
 echo "Adding config files ..."
 cp  bashrc_sshagent /home/${USER}/.bashrc_sshagent
